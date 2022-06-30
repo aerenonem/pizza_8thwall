5d810227241e3a36ba9c4bc8a9f1208c13f60eaa
aerenonem <48674540+aerenonem@users.noreply.github.com>
2022-06-30 16:05:54 +0300

    Add files via upload

---
+++ ar.js
@@ -0,0 +1,17 @@
+
+
+// function android(){
+//     var buton = document.getElementById('androidFakeButton')
+//     buton.click("https://oyungezer.com.tr/")
+// }
+
+// android();
+
+// document.getElementById("androidFakeButton").addEventListener("click",myFunction);
+
+
+// function myFunction(){
+//     document.getElementById("androidFakeButton").innerHTML = "YOU CLICKED ME!";
+// }
+
+    
